4e337779ccebca5ce94948a807d56e57288eea58
Rey112 <45634001+Rey112@users.noreply.github.com>
2020-12-18 10:40:51 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<h1>Project 4 Login</h1>
+<body>
+    <form action="index.php" method="post">
+        <input type="hidden" name="action" value="validate_login">
+        <div class="container">
+            <label for="email">Email</label>
+            <div style="text-align: center"><input type="text" name="email" id="email"></div><br>
+
+            <label for="password">Password</label>
+            <div style="text-align: center"><input type="password" name="password" id="password"></div><br>
+        </div>
+
+        <br><br>
+        <h1>Go To:</h1>
+        <div style="text-align: center"><input type="submit" value="Submit"></div>
+        <br><br>
+        <div style="text-align: center"><a href="registration.php">Register</a></div>
+        <br>
+    </form>
+
+</body>
+</html>
